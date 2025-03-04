f1dbd4efd5409f5bbb183f4ea3e5e362d43a0986
Nathan Yan <nathan_yan_alves@hotmail.com>
2025-03-04 18:18:22 -0300

    chore: init

---
+++ src/main/java/dev/nathanyan/MeetPlanner/MeetPlannerApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanyan.MeetPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetPlannerApplication.class, args);
+	}
+
+}
