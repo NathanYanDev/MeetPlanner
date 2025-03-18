3ca3c66fbdac594db429f2c40f589d70a7970268
Nathan Yan <nathan_yan_alves@hotmail.com>
2025-03-17 22:53:32 -0300

    feat: add email validation for meeting participant improve: update API response to follow REST standards

--- src/main/java/dev/nathanyan/MeetPlanner/controller/MeetingController.java
+++ src/main/java/dev/nathanyan/MeetPlanner/controller/MeetingController.java
@@ -1,17 +1,18 @@
 package dev.nathanyan.MeetPlanner.controller;
 
+import dev.nathanyan.MeetPlanner.handler.ResponseHandler;
 import dev.nathanyan.MeetPlanner.model.CreateMeetingRequest;
 import dev.nathanyan.MeetPlanner.model.Meeting;
 import dev.nathanyan.MeetPlanner.model.Participant;
 import dev.nathanyan.MeetPlanner.service.MeetingService;
 import dev.nathanyan.MeetPlanner.service.ParticipantsService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @RestController
 @RequestMapping("/meeting")
@@ -26,21 +27,42 @@ public class MeetingController {
     }
 
     @GetMapping
-    public List<Meeting> getMeetings() {
-        return meetingService.getAll();
+    public ResponseEntity<Object> getMeetings() {
+        List<Meeting> meetings = meetingService.getAll();
+        if(meetings.isEmpty()) {
+            return ResponseHandler.generateResponse("No meeting has yet been created", HttpStatus.OK, Collections.emptyList());
+        } else {
+            return ResponseHandler.generateResponse("The meetings were successfully found", HttpStatus.OK, meetings);
+        }
     }
 
     @PostMapping("/create")
     public ResponseEntity<Object> createMeeting(@RequestBody CreateMeetingRequest request) {
         Set<Participant> participants = participantsService.getParticipantsByEmail(request.getParticipantsEmail());
+        Set<String> emailsNotFound = new HashSet<>(request.getParticipantsEmail());
+
+        for(String email: participants.stream().map(Participant::getEmail).toList()) {
+            emailsNotFound.remove(email);
+        }
 
         Meeting meeting = new Meeting(null, request.getTitle(), request.getDescription(), request.getDateTime(), request.getLocation(), LocalDateTime.now(), request.getDuration(), participants);
 
-        return ResponseEntity.ok(meetingService.create(meeting));
+        try {
+            Meeting data = meetingService.create(meeting);
+
+            if(!emailsNotFound.isEmpty()) {
+                return ResponseHandler.generateResponse(String.format("The meeting was created successfully, but some emails are not found: %s", emailsNotFound), HttpStatus.CREATED, data);
+            } else {
+                return ResponseHandler.generateResponse("The meeting was created successfully", HttpStatus.CREATED, data);
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There was an error, check the fields and try again");
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable String id){
+    public ResponseEntity<Object> delete(@PathVariable String id){
         meetingService.delete(id);
+        return ResponseHandler.generateResponse("The meeting was deleted successfully", HttpStatus.OK, Collections.emptyList());
     }
 }
--- src/main/java/dev/nathanyan/MeetPlanner/controller/ParticipantsController.java
+++ src/main/java/dev/nathanyan/MeetPlanner/controller/ParticipantsController.java
@@ -1,11 +1,14 @@
 package dev.nathanyan.MeetPlanner.controller;
 
+import dev.nathanyan.MeetPlanner.handler.ResponseHandler;
 import dev.nathanyan.MeetPlanner.model.CreateParticipantRequest;
 import dev.nathanyan.MeetPlanner.model.Participant;
 import dev.nathanyan.MeetPlanner.service.ParticipantsService;
 import dev.nathanyan.MeetPlanner.types.Status;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -22,14 +25,23 @@ public class ParticipantsController {
     }
 
     @GetMapping
-    public List<Participant> getAll() {
-        return participantsService.listAll();
+    public ResponseEntity<Object> getAll() {
+        List<Participant> participants = participantsService.listAll();
+        if(participants.isEmpty()) {
+            return ResponseHandler.generateResponse("No participant has yet been created", HttpStatus.OK, Collections.emptyList());
+        } else {
+            return ResponseHandler.generateResponse("The participants were successfully found", HttpStatus.OK, participants);
+        }
     }
 
     @PostMapping("/create")
     public ResponseEntity<Object> create(@RequestBody CreateParticipantRequest request) {
         Participant participant = new Participant(null, request.getEmail(), request.getName(), Status.PENDING, LocalDateTime.now(), Collections.emptySet());
 
-        return ResponseEntity.ok(participantsService.create(participant));
+        try {
+            return ResponseHandler.generateResponse("The participant was created successfully", HttpStatus.CREATED, participant);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There was an error, check the fields and try again");
+        }
     }
 }
